c36c8570cf01a6af85e631589113bba36ef6f618
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2023-01-27 10:18:55 -0300

    implementado docker

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import CreateCategoryController from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 import multer from "multer";
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
